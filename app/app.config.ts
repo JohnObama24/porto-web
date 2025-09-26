53646d441eb2cfb68bb6fb36ea8fbcfbd22252bb
JohnObama24 <johnobama24keren@gmail.com>
2025-09-26 13:24:36 +0700

    update to nuxt

---
+++ app/app.config.ts
@@ -0,0 +1,45 @@
+export default defineAppConfig({
+  global: {
+    picture: {
+      dark: 'https://avatars.githubusercontent.com/u/159476448?v=4',
+      light: 'https://avatars.githubusercontent.com/u/159476448?v=4',
+      alt: 'My profile picture'
+    },
+    meetingLink: 'https://cal.com/',
+    email: 'ui-pro@nuxt.com',
+    available: true
+  },
+  ui: {
+    colors: {
+      primary: 'blue',
+      neutral: 'neutral'
+    },
+    pageHero: {
+      slots: {
+        container: 'py-18 sm:py-24 lg:py-32',
+        title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
+        description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
+      }
+    }
+  },
+  footer: {
+    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    colorMode: false,
+    links: [{
+      'icon': 'i-simple-icons-discord',
+      'to': 'https://go.nuxt.com/discord',
+      'target': '_blank',
+      'aria-label': 'Me on Discord'
+    }, {
+      'icon': 'i-simple-icons-instagram',
+      'to': 'https://www.instagram.com/johnobama_/',
+      'target': '_blank',
+      'aria-label': 'me on IG'
+    }, {
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/JohnObama24',
+      'target': '_blank',
+      'aria-label': 'Me on GitHub'
+    }]
+  }
+})
